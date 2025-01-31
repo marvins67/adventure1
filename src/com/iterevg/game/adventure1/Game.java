aec0f70127f75e894ead7f5afde1e4da3cfdc845
mrvs <gregmarvins@gmail.com>
2025-01-31 17:36:46 +0100

    Continue testing...

--- src/com/iterevg/game/adventure1/Game.java
+++ src/com/iterevg/game/adventure1/Game.java
@@ -5,48 +5,117 @@ import com.iterevg.game.adventure1.graphics.ScreenRenderer;
 import com.iterevg.game.adventure1.graphics.Sprite;
 
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Game {
+import static com.iterevg.game.adventure1.Constants.*;
 
-    private JFrame frame ;
+public class Game extends MouseAdapter {
+
+    private JFrame frame;
     private ScreenRenderer screenRenderer;
-
     private Image background;
-
     private Sprite sprite;
+    private volatile boolean running = true;
+
+    int mouseX = 580;
+    int mouseY = 147;
 
    public void init(){
+       screenRenderer = new ScreenRenderer();
+       screenRenderer.addMouseListener(this);
+
        frame = new JFrame(Constants.NAME);
-       screenRenderer = new ScreenRenderer(Constants.SCR_WIDTH, Constants.SCR_HEIGHT);
        frame.add(screenRenderer);
-       frame.setSize(Constants.SCR_WIDTH, Constants.SCR_HEIGHT);
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(GAME_WIDTH, GAME_HEIGHT);
+       frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
        frame.setResizable(false);
        frame.setVisible(true);
+       frame.addKeyListener(new KeyAdapter() {
+           @Override
+           public void keyPressed(KeyEvent e) {
+               if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                   running = false;
+               }
+           }
+       });
 
        background = new Image();
-       background.read("image.jpg");
+       background.read("gk_back.png");
 
        Image image = new Image();
-       image.read("sprite.png");
+       image.read("spr_gk1.png");
        sprite = new Sprite();
        sprite.setImage(image);
-       sprite.setX(400);
-       sprite.setY(400);
+       sprite.setX(580);
+       sprite.setY(147);
    }
 
-   public void run(){
-       screenRenderer.draw(background);
-       screenRenderer.draw(sprite);
-       screenRenderer.repaint();
-       update();
-   }
+    public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000.0 / Constants.FRAMERATE; // Temps par tick pour n FPS
+        double deltaTime = 0;
+        double frameRateLimit = 0;
+        int frames = 0;
 
-    private void update() {
-       //Mise à jour des données du jeu
-        sprite.setX(sprite.getX() + 1 );
-        sprite.setY(sprite.getY() + 1 );
+        while (running) {
+            long now = System.nanoTime(); // Récupérer le temps actuel
+            deltaTime += (now - lastTime); // Calculer le delta
+            frameRateLimit += (now - lastTime) / nsPerTick;
+            lastTime = now;
+
+            //On ne met à jour l'affichage que n fois par secondes (Constants.FRAMERATE)
+            while (frameRateLimit >= 1) {
+                frameRateLimit--;
+                screenRenderer.drawBackground(background.getBytes());
+                screenRenderer.drawSprite(sprite);
+                screenRenderer.repaint();
+                updateScreen();
+            }
+
+            frames++;
+
+            // Affiche les FPS toutes les secondes
+            if (deltaTime >= Constants.ONE_SECOND ){
+                deltaTime = 0;
+                screenRenderer.setFps(frames);
+                frames = 0;
+            }
+        }
     }
 
-    public void stop(){}
+    private void updateScreen() {
+       //Mise à jour des données du jeu
+        int v = 2 * SCALE;
+        boolean moveX = Math.abs(sprite.getX() - mouseX) >= v;
+        boolean moveY = Math.abs(sprite.getY() - mouseY) >= v;
+        if (moveX || moveY) {
+            System.out.println("click : " + mouseX + ", " + mouseY + " sprite : " + sprite.getX() + ", " + sprite.getY());
+            sprite.incFrame();
+            sprite.setWalk();
+            if (moveX) {
+                int dx = v * (sprite.getX() < mouseX ? 1 : -1);
+                sprite.setX(sprite.getX() + dx);
+            }
+            if (moveY) {
+                int dy = v * (sprite.getY() < mouseY ? 1 : -1);
+                sprite.setY(sprite.getY() + dy);
+            }
+        } else {
+            sprite.setNoWalk();
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    public void stop(){
+       frame.dispose();
+       System.out.println("That's all folks!");
+    }
 }
--- src/com/iterevg/game/adventure1/Main.java
+++ src/com/iterevg/game/adventure1/Main.java
@@ -8,26 +8,7 @@ public class Main extends JPanel {
     public static void main(String[] args) {
         game = new Game();
         game.init();
-        double avant = 0;
-        double apres = System.nanoTime() / 1000000000.0;
-        double duree = 0;
-        double frameTime = 0;
-        int frames = 0;
-        int fps = 0;
-        while (true) {
-            avant = System.nanoTime() / 1000000000.0;
-            game.run();
-            duree = avant - apres;
-            apres = avant;
-            frameTime += duree;
-            frames++;
-            if (frameTime >= 1.0) {
-                frameTime = 0;
-                fps = frames;
-                frames = 0;
-                System.out.println("FPS : " + fps);
-            }
-        }
-        //game.stop();
+        game.run();
+        game.stop();
     }
 }
